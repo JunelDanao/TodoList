ecba3469c08bdb069436178a8056df43b80995d5
Loin Lorenz <jhonezdanao@gmail.com>
2019-08-07 12:13:49 +0800

    Updates

---
+++ precache-manifest.655b70c261fcd9f73ddea256418a74ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e8be53a991a18cca15e75764c9b6bd3",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "c0c4b8db36ec0a48189f",
+    "url": "/TodoList/static/css/main.ba743eed.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/TodoList/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "c0c4b8db36ec0a48189f",
+    "url": "/TodoList/static/js/main.27b3b09e.chunk.js"
+  },
+  {
+    "revision": "23ef1ee30c4a1e2cab36",
+    "url": "/TodoList/static/js/runtime~main.90b1048d.js"
+  }
+]);
